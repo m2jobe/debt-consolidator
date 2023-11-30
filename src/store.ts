3e1b118db544b06e98b0cb2973837a189d9d366d
Muhammed Jobe <thejobemuhammed@gmail.com>
2023-11-30 17:51:55 -0500

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,8 @@ import { combineReducers, createStore } from 'redux'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import { CalculateReducer } from './features/calculator'
 
+//Todo: Introduce Redux Persist to persist and rehydrate store
+
 /* Create root reducer, containing all features of the application */
 const rootReducer = combineReducers({
   calculator: CalculateReducer,
